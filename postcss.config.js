ec2758051bcc3ba5c08db5b637e6fe1769d413b2
Pierre Bresson <pierre@bresson.io>
2025-05-29 12:04:45 +0200

    fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+module.exports = ({ env }) => ({
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
+    cssnano:
+      env === "production"
+        ? {
+            preset: ["default", { discardComments: { removeAll: true } }],
+          }
+        : false,
   },
-};
+});
